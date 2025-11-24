b5f59770d1e2396b34e93ff707df389e32173c63
Sona Zeynalova <sonie.zeynalova@gmail.com>
2025-11-25 00:53:07 +0400

    Update rock-paper-scissors.js

--- Projects/Rock-Paper-Scissors/rock-paper-scissors.js
+++ Projects/Rock-Paper-Scissors/rock-paper-scissors.js
@@ -15,6 +15,86 @@ updateScoreElement();
 };
 */
 
+let isAutoPlaying = false;
+let intervalId;
+
+/*const autoPlay = () => {
+  
+}*/
+
+function autoPlay() {
+  if (!isAutoPlaying) {
+    intervalId =  setInterval(() => {
+    const playerMove = pickComputerMove();
+    playGame(playerMove);
+    document.querySelector('.auto-play-button').innerHTML = 'Stop playing';
+  }, 1000);
+  isAutoPlaying = true;
+  } else {
+    clearInterval(intervalId);
+    isAutoPlaying = false;
+    document.querySelector('.auto-play-button').innerHTML = 'Auto Play';
+  }
+}
+document.querySelector('.auto-play-button').addEventListener('click', () => {resetMessage()});
+
+function resetScore() {
+  score.wins = 0;
+  score.losses = 0;
+  score.ties = 0;
+  localStorage.removeItem('score');
+  updateScoreElement();
+}
+
+function resetMessage() {
+  document.querySelector('.reset-message')
+    .innerHTML = `
+    Are you sure you want to reset the score?
+    <button class="reset-message-button yes-button">Yes</button>
+    <button class="reset-message-button no-button">No</button>`;
+  
+  document.querySelector('.yes-button').addEventListener('click', () => {
+    resetScore();
+    document.querySelector('.reset-message')
+    .innerHTML = '';
+  });
+  document.querySelector('.no-button').addEventListener('click', () => {
+    document.querySelector('.reset-message')
+    .innerHTML = '';
+  })
+}
+
+document.querySelector('.reset-button').addEventListener('click', () => {resetMessage()});
+
+document.querySelector('.js-rock-button')
+  .addEventListener('click', () => {
+    playGame('rock');
+  });
+
+document.querySelector('.js-paper-button')
+  .addEventListener('click', () => {
+    playGame('paper');
+  });
+
+document.querySelector('.js-scissors-button')
+  .addEventListener('click', () => {
+    playGame('scissors');
+  });
+
+document.body.addEventListener('keydown', (event) => {
+  if (event.key === 'r') {
+    playGame('rock');
+  } else if (event.key === 'p') {
+    playGame('paper');
+  } else if (event.key === 's') {
+    playGame('scissors');
+  } else if (event.key === 'a') {
+    autoPlay();
+  } else if (event.key === 'Backspace') {
+    resetMessage();
+  }
+})
+
 function playGame(playerMove) {
   const computerMove = pickComputerMove();
   let result = '';
@@ -40,9 +120,9 @@ function playGame(playerMove) {
     if (computerMove === 'rock') {
       result = 'You lose.'
     } else if (computerMove === 'paper') {
-      result = 'Tie.'
-    } else if (computerMove === 'scissors') {
       result = 'You win.'
+    } else if (computerMove === 'scissors') {
+      result = 'Tie.'
     }
   }
 
